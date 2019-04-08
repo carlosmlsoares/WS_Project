ca83be18e3f0d318020654205074e9a0a07cf0f4
carlosmlsoares <carlos_soares_97@hotmail.com>
2019-04-08 22:44:14 +0100

    Little problem to solve

--- TP1/urls.py
+++ TP1/urls.py
@@ -4,10 +4,11 @@ from TP1 import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('movies/', views.movies, name="movies"),
-    path('movie/(?P<id>[^/]+)$', views.movie, name="movie"),
+    path('movie/<id>', views.movie, name="movie"),
     path('actors/', views.actors, name="actors"),
-    path('actor/(?P<id>[^/]+)$', views.actor, name="actor"),
+    path('actor/<id>', views.actor, name="actor"),
     path('directors/', views.directors, name="directors"),
-    path('director/(?P<id>[^/]+)$', views.directors, name="director"),
-    path('genre/<id>', views.genre, name="genre")
+    path('director/<id>', views.director, name="director"),
+    path('genre/<id>', views.genre, name="genre"),
+    path('search/', views.search, name="search")
 ]
--- TP1/views.py
+++ TP1/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from TP1.db import sparql
 
-
 # Create your views here.
 def index(request):
     return render(request, "index.html", {
@@ -22,15 +21,55 @@ def actors(request):
 
 
 def actor(request, id):
-    return render(request, "actor.html", {'id': id})
+    query = """SELECT ?name
+                    WHERE {
+                        worker:""" + id + """ worker:name ?name.
+                        }
+                    LIMIT 1
+                    """
 
+    name = sparql(query)[0]['name']
+
+    query = """SELECT ?movie ?title ?year ?score
+            WHERE {
+                worker:""" + id + """ worker:played_on ?movie .
+                ?movie movie:title ?title.
+                ?movie movie:year ?year.
+                ?movie movie:score ?score.
+
+            }"""
+
+    content = sparql(query)
+
+    return render(request, "actor.html", {'id': id, 'name': name, 'content': content})
 
 def directors(request):
     return render(request, "directors.html", {})
 
 
 def director(request, id):
-    return render(request, "director.html", {'id': id})
+    query = """SELECT ?name
+                WHERE {
+                    worker:"""+id+""" worker:name ?name.
+                    }
+                LIMIT 1
+                """
+
+    name = sparql(query)[0]['name']
+
+    query="""SELECT ?movie ?title ?year ?score
+        WHERE {
+            worker:""" +id+""" worker:directed ?movie .
+            ?movie movie:title ?title.
+            ?movie movie:year ?year.
+            ?movie movie:score ?score.
+            
+        }"""
+
+    content = sparql(query)
+
+
+    return render(request, "director.html", {'id': id,'name':name,'content':content})
 
 
 def genre(request, id):
@@ -38,7 +77,52 @@ def genre(request, id):
 
 
 def search(request):
-    return render(request, "search.html", {})
+
+    term=request.GET['term']
+
+
+    query="""SELECT ?id ?title ?year ?score
+        WHERE {
+            ?id movie:title ?title.
+            ?id movie:year ?year.
+            ?id movie:score ?score.
+            FILTER (CONTAINS(LCASE(?title), "TERM"))
+        }
+        GROUP BY ?id ?title 
+        LIMIT 20""".replace("TERM", term)
+
+    movies=sparql(query)
+    print(movies)
+
+
+    query="""SELECT ?id ?name (COUNT(?movie) AS ?movies)
+        WHERE {
+            ?id worker:directed ?movie.
+            ?id worker:name ?name.
+            FILTER (CONTAINS(LCASE(?name), "TERM"))
+        }
+        GROUP BY ?id ?name
+        ORDER BY DESC (?movies)
+        LIMIT 20 """.replace("TERM", term)
+
+    directors = sparql(query)
+    print(directors)
+
+
+    query = """SELECT ?id ?name (COUNT(?movie) AS ?movies)
+            WHERE {
+                ?id worker:played_on ?movie.
+                ?id worker:name ?name.
+                FILTER (CONTAINS(LCASE(?name), "TERM"))
+            }
+            GROUP BY ?id ?name
+            ORDER BY DESC (?movies)
+            LIMIT 20 """.replace("TERM", term)
+
+    actors = sparql(query)
+    print(actors)
+
+    return render(request, "search.html", {'term':term,'directors':directors,'movies':movies,'actors':actors})
 
 
 def get_genres():
